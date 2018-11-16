b3f004bd21f7bbb160a33c7b5387b0dce82a76c9
jbortiz0623 <jbortiz0623@gmail.com>
2018-11-16 11:04:27 -0800

    inserted script.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,29 @@
-$(document).on("ready", function(){
+console.log('sanity');
 
-});
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        let 
+            key = `dc6zaTOxFJmzC`,
+            search = $('#search').val(),
+            url = `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${key}&limit=5`
+
+        onSuccess = (res) => {
+            $('.gif-gallery').empty();
+            res.data.forEach(gif => {
+                let elem = `<img src=${gif.images.original.url} atl=${gif.title}/>`
+                $('.gif-gallery').append(elem);
+            });
+            }
+    
+        onError = (e1,e2,e3) => {
+                console.log(e2);
+            }
+
+        $.ajax({
+            method: 'GET',
+            url: url,
+            success: onSuccess,
+            error: onError,
+        });
+
+    });
